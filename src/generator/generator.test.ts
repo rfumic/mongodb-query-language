ab1246b385206ce00c9f4918755fd74dbe191448
rfumic <rokofumic@gmail.com>
2025-04-10 19:05:43 +0200

    add IN and NOT IN

--- src/generator/generator.test.ts
+++ src/generator/generator.test.ts
@@ -3,12 +3,37 @@ import { Tokenizer } from "../tokenizer/tokenizer";
 import { generateQuery } from "./generator";
 
 describe("Generator", () => {
-	test("Test creating basic queries", () => {
+	test("Test creating basic comparison queries", () => {
 		const inputs = [
 			{ inputString: "age > 18", expected: { age: { $gt: 18 } } },
 			{ inputString: "age >= 18", expected: { age: { $gte: 18 } } },
 			{ inputString: "age < 18.11", expected: { age: { $lt: 18.11 } } },
 			{ inputString: "age <= 18", expected: { age: { $lte: 18 } } },
+			{
+				inputString: "field1 > field2",
+				expected: { $expr: { $gt: ["field1", "field2"] } },
+			},
+			{
+				inputString: "field1 >= field2",
+				expected: { $expr: { $gte: ["field1", "field2"] } },
+			},
+			{
+				inputString: "field1 < field2",
+				expected: { $expr: { $lt: ["field1", "field2"] } },
+			},
+			{
+				inputString: "field1 <= field2",
+				expected: { $expr: { $lte: ["field1", "field2"] } },
+			},
+			{
+				inputString: "field IN (11, 123.1, 'string')",
+				expected: { field: { $in: [11, 123.1, "string"] } },
+			},
+			// TODO: add when NOT IN starts working
+			// {
+			// 	inputString: "field NOT IN (11, 123.1, 'string')",
+			// 	expected: { field: { $nin: [11, 123.1, "string"] } },
+			// },
 		];
 
 		for (const input of inputs) {
--- src/generator/generator.ts
+++ src/generator/generator.ts
@@ -1,9 +1,10 @@
 import {
-	type Identifier,
-	isComparisonExpression,
 	type ASTNode,
+	type Identifier,
+	type Literal,
+	isComparisonExpression,
 	isIdentifier,
-    Literal,
+	isInExpression,
 } from "../ast/ast";
 
 // TODO: move this to a separate file later
@@ -13,44 +14,75 @@ const mongoOperatorTable: Record<string, string> = {
 	">": "$gt",
 	">=": "$gte",
 	"=": "$eq", // TODO: check this
+	"!=": "$ne",
+	IN: "$in",
+	"NOT IN": "$nin",
 };
 
 export function generateQuery(tree: ASTNode) {
-	console.log(tree);
+	console.log("tree:", tree);
+	if (isInExpression(tree)) {
+		const values = tree.values.map((literal) => literal.value);
+		const operator = mongoOperatorTable[tree.operator];
+
+		if (!operator) {
+			// TODO: handle error
+			return;
+		}
+
+		return {
+			[tree.field.name]: {
+				[operator]: values,
+			},
+		};
+	}
 	if (isComparisonExpression(tree)) {
 		const leftIsIdentifier = isIdentifier(tree.left);
 		const rightIsIdentifier = isIdentifier(tree.right);
 
 		if (leftIsIdentifier && rightIsIdentifier) {
 			// TODO: use $expr here
-			return;
+			const leftSide = tree.left as Identifier;
+			const rightSide = tree.right as Identifier;
+			const operator = mongoOperatorTable[tree.operator];
+			if (!operator) {
+				// TODO: handle error
+				return;
+			}
+			return {
+				$expr: {
+					[operator]: [leftSide.name, rightSide.name],
+				},
+			};
 		}
 
 		let field: Identifier;
-        let literal: Literal;
+		let literal: Literal;
 		if (leftIsIdentifier) {
 			field = tree.left as Identifier;
-            // TODO: check if is literal
-            literal = tree.right as Literal;
+			// TODO: check if is literal
+			literal = tree.right as Literal;
 		} else if (rightIsIdentifier) {
 			field = tree.right as Identifier;
-            // TODO: check if is literal
-            literal = tree.left as Literal;
+			// TODO: check if is literal
+			literal = tree.left as Literal;
 		} else {
 			// TODO: handle error
 			return;
 		}
 
-        const operator = mongoOperatorTable[tree.operator];
-        if(!operator){
-            // TODO: handle error
-            return;
-        }
-        return {
-            [field.name]: {
-                [operator]: literal.value
-            }
-        }
-
+		// TODO:
+		// maybe extract to function that returns like:
+		//     {$gte: [left, right]}
+		const operator = mongoOperatorTable[tree.operator];
+		if (!operator) {
+			// TODO: handle error
+			return;
+		}
+		return {
+			[field.name]: {
+				[operator]: literal.value,
+			},
+		};
 	}
 }
